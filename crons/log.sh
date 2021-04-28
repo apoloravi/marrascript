91dafaa59b72ab4810c023c8677cd3693fde3589
apoloravi <34797030+apoloravi@users.noreply.github.com>
2021-04-28 17:55:57 -0300

    Update log.sh

--- crons/log.sh
+++ crons/log.sh
@@ -1,1 +1,8 @@
 
+#Clear da Marra Cloud
+echo > /var/log/syslog
+echo > /var/log/auth.log
+echo > /var/log/freeradius/radius.log
+
+# Kill processos alem de 3m
+killall -y 3m /usr/local/mkauth/backup/update

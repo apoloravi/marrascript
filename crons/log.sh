f34a72c66250d2559f7a3688f1119c7747f5ce1d
apoloravi <34797030+apoloravi@users.noreply.github.com>
2021-04-28 18:08:41 -0300

    Update log.sh

--- crons/log.sh
+++ crons/log.sh
@@ -5,4 +5,4 @@ echo > /var/log/auth.log
 echo > /var/log/freeradius/radius.log
 
 # Kill processos alem de 3m
-killall -y 3m /usr/local/mkauth/backup/update
+killall -y 3h /usr/local/mkauth/backup/update

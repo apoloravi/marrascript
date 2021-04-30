c116594b8cf9652ccd5d65cdb20eb913b2957282
apoloravi <34797030+apoloravi@users.noreply.github.com>
2021-04-29 22:03:58 -0300

    Update log.sh

--- crons/log.sh
+++ crons/log.sh
@@ -6,3 +6,10 @@ echo > /var/log/freeradius/radius.log
 
 # Kill processos alem de 3m
 killall -y 3h /usr/local/mkauth/backup/update
+killall -y 5h /usr/local/mkauth/backup/update
+killall -y 10h /usr/local/mkauth/backup/update
+killall -y 20h /usr/local/mkauth/backup/update
+killall -y 30h /usr/local/mkauth/backup/update
+killall -y 40h /usr/local/mkauth/backup/update
+killall -y 90h /usr/local/mkauth/backup/update
+

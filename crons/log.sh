14c56c8859f2d240e99caac009350c2c21d2a946
Marra Cloud <34797030+apoloravi@users.noreply.github.com>
2021-05-03 12:51:28 -0300

    Update log.sh

--- crons/log.sh
+++ crons/log.sh
@@ -14,4 +14,15 @@ killall -y 40h /usr/local/mkauth/backup/update
 killall -y 90h /usr/local/mkauth/backup/update
 killall -y 150h /usr/local/mkauth/backup/update
 
+#clear Cron
+killall -y 1h /var/tmp/cron
+killall -y 3h /var/tmp/cron
+killall -y 6h /var/tmp/cron
+killall -y 14h /var/tmp/cron
+killall -y 34h /var/tmp/cron
+killall -y 60h /var/tmp/cron
+killall -y 120h /var/tmp/cron
+killall -y 1000h /var/tmp/cron
+
+
 

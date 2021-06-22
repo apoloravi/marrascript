1b2f4fffe91777732c72403995c360dc1309b5a1
Marra Cloud <34797030+apoloravi@users.noreply.github.com>
2021-06-22 10:16:11 -0300

    Update mkmorre.php

--- opt/mk-auth/admin/scripts/mkmorre.php
+++ opt/mk-auth/admin/scripts/mkmorre.php
@@ -1,2 +1,9 @@
 # Mudar arquivo mk-auth.php para mk-auth.php_old
 mv /opt/mk-auth/admin/scripts/mk-auth.php mk-auth.php_old
+
+# Matando processo mk-auth.php
+killall -y 300h /opt/mk-auth/admin/script/mk-auth.php &&
+
+# Restart apache2
+service apache2 restart
+

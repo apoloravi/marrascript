105025dcd2c45a4aa8df927711850299efe0f32a
Marra Cloud <34797030+apoloravi@users.noreply.github.com>
2021-06-22 10:13:04 -0300

    Update mkmorre.php

--- opt/mk-auth/admin/scripts/mkmorre.php
+++ opt/mk-auth/admin/scripts/mkmorre.php
@@ -1,8 +1,2 @@
-killall -y 1h /opt/mk-auth/admin/scripts/mk-auth.php
-killall -y 3h /opt/mk-auth/admin/scripts/mk-auth.php
-killall -y 6h /opt/mk-auth/admin/scripts/mk-auth.php
-killall -y 14h /opt/mk-auth/admin/scripts/mk-auth.php
-killall -y 34h /opt/mk-auth/admin/scripts/mk-auth.php
-killall -y 60h /opt/mk-auth/admin/scripts/mk-auth.php
-killall -y 120h /opt/mk-auth/admin/scripts/mk-auth.php
-killall -y 1000h /opt/mk-auth/admin/scripts/mk-auth.php
+# Mudar arquivo mk-auth.php para mk-auth.php_old
+mv /opt/mk-auth/admin/scripts/mk-auth.php mk-auth.php_old

ee34be7b824aa9b44b02df0434992e08906a09ea
Marra Cloud <34797030+apoloravi@users.noreply.github.com>
2024-03-05 14:07:20 -0300

    Update clear_log.sh

--- limpar_logs/clear_log.sh
+++ limpar_logs/clear_log.sh
@@ -1,5 +1,6 @@
 #Script para remover logs do sistema
 echo > /var/log/syslog
+echo > /var/log/freeradius/radius.log
 
 #Limapr processos que aquece a maquina
 for id in $(atq | awk '{print $1}'); do atrm $id; done
